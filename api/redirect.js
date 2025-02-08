3969337d90a3642d917ff54946d721445e06610a
Solutionandmore <thien747@thienoi.vn>
2025-02-09 04:08:35 +0700

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,8 +1,8 @@
 export default function handler(req, res) {
-    const appId = "YOUR_APP_ID";
-    const oaId = "YOUR_OA_ID";
-    const redirectUri = encodeURIComponent("olaz-git-master-phuoc-thien-les-projects.vercel.app");
-    const authUrl = `https://oauth.zaloapp.com/v4/oa/permission?app_id=${appId}&oa_id=${oaId}&redirect_uri=${redirectUri}&code_challenge=YOUR_CODE_CHALLENGE`;
+    const appId = "2660693949023071086";  // Thay bằng App ID của bạn
+    const redirectUri = encodeURIComponent("https://olaz-git-master-phuoc-thien-les-projects.vercel.app/api/zaloCallback");
+
+    const authUrl = `https://oauth.zaloapp.com/v4/oa/permission?app_id=${appId}&redirect_uri=${redirectUri}`;
 
     res.writeHead(302, { Location: authUrl });
     res.end();
