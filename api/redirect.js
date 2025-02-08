4eccbf62c7333e2f460048feec50d88009ab8429
Solutionandmore <thien747@thienoi.vn>
2025-02-09 03:46:05 +0700

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     const appId = "YOUR_APP_ID";
     const oaId = "YOUR_OA_ID";
-    const redirectUri = encodeURIComponent("https://olaz.vercel.app/api/callback");
+    const redirectUri = encodeURIComponent("olaz-git-master-phuoc-thien-les-projects.vercel.app");
     const authUrl = `https://oauth.zaloapp.com/v4/oa/permission?app_id=${appId}&oa_id=${oaId}&redirect_uri=${redirectUri}&code_challenge=YOUR_CODE_CHALLENGE`;
 
     res.writeHead(302, { Location: authUrl });
