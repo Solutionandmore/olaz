c64f713652465eb724db5c4aeaba004d14ccfd09
Solutionandmore <thien747@thienoi.vn>
2025-02-10 00:08:12 +0700

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     const appId = "2660693949023071086";  // Thay bằng App ID của bạn
-    const redirectUri = encodeURIComponent("https://olaz-git-master-phuoc-thien-les-projects.vercel.app/api/zaloCallback");
+    const redirectUri = encodeURIComponent("https://olaz-phuoc-thien-les-projects.vercel.app/api/zaloCallback");
 
     const authUrl = `https://oauth.zaloapp.com/v4/oa/permission?app_id=${appId}&redirect_uri=${redirectUri}`;
 
