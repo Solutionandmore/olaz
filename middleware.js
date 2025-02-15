8c93bd8092c94c16d43b5971265e26f9bf7c04c9
Solutionandmore <thien747@thienoi.vn>
2025-02-15 15:53:58 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,17 +1,9 @@
-import { get } from '@vercel/edge-config';
 import { NextResponse } from 'next/server';
 
-export async function middleware(req) {
-  // Kiểm tra nếu đường dẫn là '/redirect'
+export function middleware(req) {
   if (req.nextUrl.pathname === '/redirect') {
-    // Lấy URL cấp quyền Zalo từ Edge Config
-    const zaloOAuthUrl = await get('zaloOAuthUrl');
-    if (zaloOAuthUrl) {
-      // Redirect ngay tại Edge
-      return NextResponse.redirect(zaloOAuthUrl);
-    }
-    // Nếu không tìm thấy key, có thể trả về lỗi hoặc xử lý mặc định
-    return NextResponse.next();
+    const zaloOAuthUrl = "https://oauth.zaloapp.com/v4/oa/permission?app_id=2660693949023071086&redirect_uri=https%3A%2F%2Folaz-phuoc-thien-les-projects.vercel.app%2Fapi%2FzaloCallback";
+    return NextResponse.redirect(zaloOAuthUrl);
   }
   return NextResponse.next();
 }
