f8da8169c745d784aa5cc10d9c43156500ffa90a
Solutionandmore <thien747@thienoi.vn>
2025-02-15 15:36:27 +0700

    Delete api/redirect.js

--- api/redirect.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-    const appId = "2660693949023071086";  // Thay bằng App ID của bạn
-    const redirectUri = encodeURIComponent("https://olaz-phuoc-thien-les-projects.vercel.app/api/zaloCallback");
-
-    const authUrl = `https://oauth.zaloapp.com/v4/oa/permission?app_id=${appId}&redirect_uri=${redirectUri}`;
-
-    res.writeHead(302, { Location: authUrl });
-    res.end();
-}
